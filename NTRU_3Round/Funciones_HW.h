e16c48044bd2c2ac71f86b7389b260ecc136e59c
Eros Camacho <eroscamaru@gmail.com>
2023-09-24 11:02:50 +0000

    Deleting SW

--- NTRU_3Round/Funciones_HW.h
+++
@@ -1,6 +0,0 @@
-
-void load_ntru_hw(int DBG, int M, int max_cycles);
-void ntru_mult_ms2xs(uint16_t* a, uint16_t* b, uint16_t* c,
-	PYNQ_SHARED_MEMORY shared_memory_1,
-	PYNQ_SHARED_MEMORY shared_memory_2,
-	PYNQ_AXI_DMA dma, int DBG);
